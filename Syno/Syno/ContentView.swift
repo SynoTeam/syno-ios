6354399e7eab930172e63f70ffcb32c6d5ce4cb4
Seungjin Lee <sjin3687@gmail.com>
2026-06-27 17:44:21 +0900

    chore: 프로젝트 생성

---
+++ Syno/Syno/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Syno
+//
+//  Created by 이승진 on 6/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Syno/Syno/SynoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SynoApp.swift
+//  Syno
+//
+//  Created by 이승진 on 6/27/26.
+//
+
+import SwiftUI
+
+@main
+struct SynoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

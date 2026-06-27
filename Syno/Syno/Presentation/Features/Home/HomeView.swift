89112c42cc2bc62bc19ae189602abcfef7eb0c6c
Seungjin Lee <sjin3687@gmail.com>
2026-06-27 23:36:53 +0900

    chore: 프로젝트 구조 설정

--- Syno/Syno/Presentation/Features/Home/HomeView.swift
+++ Syno/Syno/Presentation/Features/Home/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  Syno
 //
 //  Created by 이승진 on 6/27/26.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    HomeView()
 }

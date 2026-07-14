5c5cf9a0eb15ca57a11f260afe4e3dd58c44b804
Seungjin Lee <sjin3687@gmail.com>
2026-07-14 15:07:52 +0900

    [#2] feat: TabView & 기본 뷰 구성

---
+++ Syno/Syno/Presentation/Features/AddContact/AddContactView.swift
@@ -0,0 +1,22 @@
+//
+//  AddContactView.swift
+//  Syno
+//
+//  Created by 이승진 on 7/14/26.
+//
+
+import SwiftUI
+
+struct AddContactView: View {
+  var body: some View {
+    VStack {
+      Text("AddContactView 입니다.")
+    }
+  }
+}
+
+#Preview {
+  NavigationStack {
+    AddContactView()
+  }
+}
---
+++ Syno/Syno/Presentation/Features/Contacts/ContactsView.swift
@@ -0,0 +1,20 @@
+//
+//  ContactsView.swift
+//  Syno
+//
+//  Created by 이승진 on 7/14/26.
+//
+
+import SwiftUI
+
+struct ContactsView: View {
+  var body: some View {
+    Text("ContactsView 입니다.")
+  }
+}
+
+#Preview {
+  NavigationStack {
+    ContactsView()
+  }
+}
